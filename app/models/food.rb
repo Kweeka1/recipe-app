669a4f25c1816a8993244e5e6b54a19d01b48cb2
kweeka <mtalbi29@yahoo.com>
2023-03-14 14:13:38 +0100

    Add relations

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,11 @@
 class Food < ApplicationRecord
+  belongs_to :user
+  has_many :recipe_foods
+
+  private
+
+  def increment_total_food_items(recipe)
+    recipe.total_food_items.increment
+    recipe.save
+  end
 end
