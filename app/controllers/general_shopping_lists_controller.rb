e8bd6fc993e8324f52c533297b079f637eec9676
kweeka <mtalbi29@yahoo.com>
2023-03-16 13:09:15 +0100

    Move sum method to application_helper.rb

--- app/controllers/general_shopping_lists_controller.rb
+++ app/controllers/general_shopping_lists_controller.rb
@@ -1,16 +1,5 @@
 class GeneralShoppingListsController < ApplicationController
-  def sum(array)
-    sum = 0
-    array.each do |number|
-      sum += number.quantity * number.food.price
-    end
-    sum
-  end
-
   def index
-    @recipe = Recipe.find(params[:recipe_id])
-    @recipe_foods = RecipeFood.all
-    @food = Food.all
-    @total_price = sum(@recipe.recipe_foods.includes([:food]))
+    @recipe_foods = RecipeFood.all.includes(:recipe, :food).where(recipe_id: params[:recipe_id])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def sum(array)
+    sum = 0
+    array.each do |number|
+      sum += number.quantity * number.food.price
+    end
+    sum
+  end
 end
