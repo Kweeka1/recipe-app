a37a15a890102d1fbde12b318e1ce7933222b560
kweeka <mtalbi29@yahoo.com>
2023-03-17 15:30:26 +0100

    Update validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,6 +7,5 @@ class Recipe < ApplicationRecord
   validates :preparation_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :cooking_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :description, presence: true
-  validates :public, presence: true, inclusion: { in: [true, false] }
-  validates :public, presence: true, exclusion: nil
+  validates :public, inclusion: { in: [true, false] }, allow_blank: true
 end
