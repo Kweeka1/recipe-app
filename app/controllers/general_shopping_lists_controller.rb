68fe891bfa4930be6820fa6cfce02e748c788c5c
kweeka <mtalbi29@yahoo.com>
2023-03-17 10:33:51 +0100

    Update query

--- app/controllers/general_shopping_lists_controller.rb
+++ app/controllers/general_shopping_lists_controller.rb
@@ -1,5 +1,7 @@
 class GeneralShoppingListsController < ApplicationController
   def index
-    @recipe_foods = RecipeFood.all.includes(:recipe, :food).where(recipe_id: params[:recipe_id])
+    @recipe_foods = RecipeFood.select("MIN(id) AS id, SUM(quantity) AS quantity, recipe_id, food_id")
+                                                 .where(recipe_id: params[:recipe_id])
+                                                 .group(:recipe_id, :food_id)
   end
 end
